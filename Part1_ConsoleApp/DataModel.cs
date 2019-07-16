082482ff03b0981443db3bc87bde4a4aa06633a8
Wes <thewbman@gmail.com>
2019-07-16 23:58:48 +0000

    Complete Part 1. Console Application

---
+++ Part1_ConsoleApp/DataModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1_ConsoleApp
+{
+    class DataModel
+
+    {
+
+        public DataModel()
+        {
+            TaskList = new TaskTracker[2];
+        }
+        private string _StringListOfNumbers;
+        public string StringListOfNumbers
+        {
+            get
+            {
+                return _StringListOfNumbers;
+            }
+            set
+            {
+                _StringListOfNumbers = value;
+            }
+        }
+
+        public List<Int32> NumberList;
+
+        public bool ParseStringIntoList()
+        {
+            //Parse the comma seperated string list into int
+            //Returns bool true when success, otherwise false
+            this.NumberList = new List<int>();
+            string[] stringArray = StringListOfNumbers.Split(',');
+
+            foreach (var n in stringArray)
+            {
+                int i;
+                if (Int32.TryParse(n, out i))
+                    this.NumberList.Add(i);
+                else
+                {
+                    this.NumberList.Clear();
+                    return false;
+                }
+
+            }
+
+            return true;
+
+        }
+
+        public int GetEvenSum()
+        {
+            int s = 0;
+
+            foreach (var n in NumberList)
+            {
+                if ((n % 2) == 0)
+                    s += n;
+            }
+
+            return s;
+        }
+
+        private string _GetEndpoint;
+        public string GetEndpoint
+        {
+            get
+            {
+                return _GetEndpoint;
+            }
+            set
+            {
+                _GetEndpoint = value;
+            }
+        }
+
+        private int _DelayMilliseconds;
+
+
+        public int DelayMilliseconds
+        {
+            get
+            {
+                return _DelayMilliseconds;
+            }
+            set
+            {
+                _DelayMilliseconds = value;
+            }
+        }
+
+        public void StartNewTask(int taskNum, string taskDisplayName)
+        {
+            if (taskNum <= TaskList.Length)
+            {
+                TaskList[taskNum - 1] = new TaskTracker(taskDisplayName,DateTime.Now);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("taskNum out of range");
+            }
+        }
+
+        public void CompleteTask(int taskNum)
+        {
+            if (taskNum <= TaskList.Length)
+            {
+                TaskList[taskNum - 1].EndTime = DateTime.Now;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("taskNum out of range");
+            }
+        }
+
+        private TaskTracker[] TaskList;
+
+
+
+        class TaskTracker
+        {
+            public string name;
+            public DateTime StartTime;
+            public DateTime EndTime;
+
+            public TaskTracker()
+            {
+            }
+
+            public TaskTracker(string name, DateTime startTime)
+            {
+                this.name = name;
+                StartTime = startTime;
+            }
+        }
+
+        public string SummarizeTaskTracker()
+        {
+            //Print out summary of parallel tasks run times
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            sb.AppendLine("");
+
+            foreach (var t in TaskList.OrderBy( c => c.EndTime).ToArray())
+            {
+                sb.AppendLine("Task " + t.name + " started " + t.StartTime.ToString("s") + " and finished " + t.EndTime.ToString() + (i == 0 ? " which was first" : ""));
+                i++;
+            }
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Part1_ConsoleApp/Program.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Part1_ConsoleApp
+{
+    class Program
+    {
+        public Timer t1;
+
+        static int Main(string[] args)
+        {
+            DateTime start = DateTime.UtcNow;
+            Log("Starting Console: " + start.ToString());
+
+            DataModel d = new DataModel();
+
+            foreach (var c in ConfigurationManager.AppSettings)
+            {
+                Log("We have app.config value for '" + c.ToString() + "' with value '" + ConfigurationManager.AppSettings[c.ToString()] + "'");
+            }
+
+            Log("");
+
+            var app = new CommandLineApplication();
+            var argList = app.Option("-l | --list <value>", "List of numbers seperated by commas (e.g.  '--list 1,2,3,4,5')", CommandOptionType.SingleValue);
+            var argGetEndpoint = app.Option("-g | --get <value>", "API endpoint to call a GET request to (e.g.  '--get http://worldtimeapi.org/api/timezone/Europe/London.json')", CommandOptionType.SingleValue);
+            var argDelayMilliseconds = app.Option("-d | --delay <value>", "Delay interval in milliseconds between printing out numbers (e.g.  '--delay 200')", CommandOptionType.SingleValue);
+            app.HelpOption("-? | -h | --help");
+
+            app.OnExecute(() =>
+            {
+                //Parse list and sum evens
+                d.StringListOfNumbers = argList.HasValue() ? argList.Value() : "1,2,3,4,5,6,7,8,9";
+                if (d.ParseStringIntoList())
+                {
+                    Log("Using list of: '" + d.StringListOfNumbers + "' with length " + d.NumberList.Count.ToString());
+
+                    Log("The sum of all even numbers in the list is: " + d.GetEvenSum().ToString());
+
+                    Log("");
+
+                }
+                else
+                {
+                    Log("Error parsing list into integers, please check your input: '" + d.StringListOfNumbers + "'");
+
+                    return -1;
+                }
+
+                //Call REST endpoint
+                d.GetEndpoint = argGetEndpoint.HasValue() ? argGetEndpoint.Value() : "http://worldtimeapi.org/api/timezone/Europe/London.json";
+                Log("Calling endpoint: " + d.GetEndpoint);
+                Log("Response: " + GetWebText(d.GetEndpoint));
+                Log("");
+
+                //Print out numbers from list with a delay between each
+                //Need to confirm is int 
+                int _delay;
+                if (!argDelayMilliseconds.HasValue())
+                {
+                    //Default
+                    _delay = 200;
+                }
+                else if (int.TryParse(argDelayMilliseconds.Value(), out _delay))
+                {
+                    //Success parsed into int
+                }
+                else
+                {
+                    Log("Error parsing delay milliseconds into integer, please check your input: '" + argDelayMilliseconds.Value() + "'");
+
+                    return -1;
+                }
+
+                Task t_user = Task.Factory.StartNew(() =>PrintListWithDelay(d.NumberList.ToArray(), _delay, "User chosen delay " + _delay.ToString() + "ms: ", false));
+                Task.WaitAll(t_user);
+                Log("");
+
+
+                //Run multiple threads
+                List<Task> taskList = new List<Task>();
+
+                Task t1 = Task.Factory.StartNew(() => 
+                {
+                    d.StartNewTask(1,"t1");
+                    PrintListWithDelay(d.NumberList.ToArray(), 500, "t1 - Delayed 500ms: ", false);
+                    d.CompleteTask(1);
+                });
+                taskList.Add(t1);
+                Task t2 = Task.Factory.StartNew(() =>
+                {
+                    d.StartNewTask(2,"t2");
+                    PrintListWithDelay(d.NumberList.ToArray(), 1000, "t2 - Delayed 1000ms in reverse: ", true);
+                    d.CompleteTask(2);
+                });
+                taskList.Add(t2);
+
+
+                Task.WaitAll(taskList.ToArray());
+
+                Log(d.SummarizeTaskTracker());
+
+
+                Log("Program is complete.  Press any key to quit");
+                Console.Read();
+
+                return 0;
+            });
+
+
+            return app.Execute(args);
+
+        }
+
+        static public void Log(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static public string GetWebText(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                //request.ContentType = "application/json";
+                //request.Accept = "application/json";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(resStream);
+                return reader.ReadToEnd();
+            }
+            catch (UriFormatException uriEx)
+            {
+                return "URI exception: " + uriEx.Message;
+            }
+            catch (WebException webEx)
+            {
+                return "Web exception: " + webEx.Message;
+            }
+        }
+
+        static public int PrintListWithDelay(int[] myIntArray, int myDelayMilliseconds, string textPreface, bool reverseList)
+        {
+            int ErrorCount = 0;
+
+            if (reverseList)
+                myIntArray = myIntArray.Reverse<int>().ToArray();
+
+            foreach(var i in myIntArray)
+            {
+                Log(textPreface + i.ToString());
+                Thread.Sleep(myDelayMilliseconds);
+            }
+
+
+            return ErrorCount;
+
+        }
+
+    }
+}
